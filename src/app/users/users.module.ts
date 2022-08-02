73b12f3f8437a7285021c411a422fc6e476ee569
Javier Ramirez Roa <javier.roa@beckshybrids.com>
2022-08-02 11:45:47 -0700

    Added flex-layout to project

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { MaterialModule } from '../material/material.module';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 
 @NgModule({
   declarations: [UsersComponent],
-  imports: [CommonModule, MaterialModule, UsersRoutingModule],
+  imports: [CommonModule, MaterialModule, FlexLayoutModule, UsersRoutingModule],
 })
 export class UsersModule {}
